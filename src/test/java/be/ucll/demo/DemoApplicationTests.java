fbf85dc5a531bc10d549f0bdbad5a6c30ba22635
RafaelBackx <43341347+RafaelBackx@users.noreply.github.com>
2020-03-23 16:07:40 +0100

    added test for taskService

--- src/test/java/be/ucll/demo/DemoApplicationTests.java
+++ src/test/java/be/ucll/demo/DemoApplicationTests.java
@@ -1,13 +1,17 @@
 package be.ucll.demo;
 
+import be.ucll.demo.DB.TaskService;
+import be.ucll.demo.Domain.Task;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
 	void contextLoads() {
-	}
 
+	}
 }
